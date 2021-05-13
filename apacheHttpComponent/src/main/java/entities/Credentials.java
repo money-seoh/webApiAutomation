9909512099d8b97a411fe5eb914ada8d8a7fc349
Shubham <thakurshubham546@gmail.com>
2021-05-13 20:44:58 +0530

    token removed

--- apacheHttpComponent/src/main/java/entities/Credentials.java
+++ apacheHttpComponent/src/main/java/entities/Credentials.java
@@ -1,7 +1,7 @@
 package entities;
 
 public class Credentials {
-    public static final String TOKEN = "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";
+    public static final String TOKEN = "";
     public static final String ID = "money-seoh";
     public static final String REPO = "webApiAutomation";
     public static final String PRIVATE_REPO = "privateApiTestRepo";
